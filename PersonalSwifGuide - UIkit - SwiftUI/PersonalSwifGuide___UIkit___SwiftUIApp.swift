84481e0b25cbd4a40d9d020b42541ff601d6c09f
BrunoSabadini <bruno_sabadini@hotmail.com>
2022-09-21 17:23:51 -0300

    Initial Commit

---
+++ PersonalSwifGuide - UIkit - SwiftUI/PersonalSwifGuide___UIkit___SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalSwifGuide___UIkit___SwiftUIApp.swift
+//  PersonalSwifGuide - UIkit - SwiftUI
+//
+//  Created by Bruno Sabadini on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PersonalSwifGuide___UIkit___SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
